cfe761e56e9893079d82790570ed4d563147b682
Ad1sKa <adskama828@gmail.com>
2026-03-10 20:50:09 +0300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-# Токен от @BotFather
-TOKEN = "8235277253:AAGA2-ssGt4_qQt25VFi6MDZwWh1b8aTC-s"
-
-# Твой ID (узнай в @userinfobot), чтобы бот знал, кто тут главный
-ADMIN_ID = 6762780884 
-
-# Логин поддержки (без @)
-SUPPORT_LINK = "ad1ska_pubg"
-
