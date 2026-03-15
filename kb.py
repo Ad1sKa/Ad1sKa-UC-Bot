b9d5b6480d0777a7cfe48a5a74d2c6f157e4daf7
Ad1sKa <adskama828@gmail.com>
2026-03-15 11:40:56 +0300

    Update kb.py

--- kb.py
+++ kb.py
@@ -14,12 +14,12 @@ main_menu = ReplyKeyboardMarkup(
 # Витрина корзины
 buy_tokens = InlineKeyboardMarkup(
     inline_keyboard=[
-        [InlineKeyboardButton(text="📦 325 UC — 380₽", callback_data="cart_add_325_380")],
-        [InlineKeyboardButton(text="📦 660 UC — 790₽", callback_data="cart_add_660_790")],
-        [InlineKeyboardButton(text="📦 1800 UC — 1990₽", callback_data="cart_add_1800_1990")],
-        [InlineKeyboardButton(text="📦 3850 UC — 3825₽", callback_data="cart_add_3850_3825")],
-        [InlineKeyboardButton(text="📦 8100 UC — 7600₽", callback_data="cart_add_8100_7600")],
-        [InlineKeyboardButton(text="📦 12610 UC — 11390₽", callback_data="cart_add_12610_11390")],
+        [InlineKeyboardButton(text="📦 60 UC — 80₽", callback_data="cart_add_60_80")],
+        [InlineKeyboardButton(text="📦 325 UC — 405₽", callback_data="cart_add_325_405")],
+        [InlineKeyboardButton(text="📦 660 UC — 800₽", callback_data="cart_add_660_800")],
+        [InlineKeyboardButton(text="📦 1800 UC — 2000₽", callback_data="cart_add_1800_2000")],
+        [InlineKeyboardButton(text="📦 3850 UC — 3925₽", callback_data="cart_add_3925")],
+        [InlineKeyboardButton(text="📦 8100 UC — 8000₽", callback_data="cart_add_8100_8000")],
         [InlineKeyboardButton(text="🛒 Оформить заказ", callback_data="cart_checkout")],
         [InlineKeyboardButton(text="🗑 Очистить корзину", callback_data="cart_clear")]
     ]
