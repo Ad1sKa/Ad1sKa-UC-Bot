64d23d71d1b1a442c82b168911a2cadf88241979
Ad1sKa <adskama828@gmail.com>
2026-03-18 14:10:26 +0300

    Update kb.py

--- kb.py
+++ kb.py
@@ -11,28 +11,30 @@ main_menu = ReplyKeyboardMarkup(
     resize_keyboard=True
 )
 
-# Витрина корзины
+# Витрина UC
 buy_tokens = InlineKeyboardMarkup(
     inline_keyboard=[
-        [InlineKeyboardButton(text="📦 60 UC — 80₽", callback_data="cart_add_60_80")],
-        [InlineKeyboardButton(text="📦 325 UC — 400₽", callback_data="cart_add_325_400")],
-        [InlineKeyboardButton(text="📦 660 UC — 750₽", callback_data="cart_add_660_750")],
-        [InlineKeyboardButton(text="📦 1800 UC — 1920₽", callback_data="cart_add_1800_1920")],
-        [InlineKeyboardButton(text="📦 3850 UC — 3800₽", callback_data="cart_add_3850_3800")],
-        [InlineKeyboardButton(text="📦 8100 UC — 7400₽", callback_data="cart_add_8100_7400")],
-        [InlineKeyboardButton(text="📦 16200 UC — 15200₽", callback_data="cart_add_16200_15200")],
-        [InlineKeyboardButton(text="📦 24300 UC — 22700₽", callback_data="cart_add_24300_22700")],
-        [InlineKeyboardButton(text="📦 32400 UC — 30000₽", callback_data="cart_add_32400_30000")],
-        [InlineKeyboardButton(text="📦 40500 UC — 38000₽", callback_data="cart_add_40500_38000")],
-        [InlineKeyboardButton(text="🛒 Оформить заказ", callback_data="cart_checkout")],
-        [InlineKeyboardButton(text="🗑 Очистить корзину", callback_data="cart_clear")]
+        [InlineKeyboardButton(text="📦 60 UC — 80₽", callback_data="buy_60_80")],
+        [InlineKeyboardButton(text="📦 325 UC — 400₽", callback_data="buy_325_400")],
+        [InlineKeyboardButton(text="📦 660 UC — 750₽", callback_data="buy_660_750")],
+        [InlineKeyboardButton(text="📦 1800 UC — 1920₽", callback_data="buy_1800_1920")],
+        [InlineKeyboardButton(text="📦 3850 UC — 3800₽", callback_data="buy_3850_3800")],
+        [InlineKeyboardButton(text="📦 8100 UC — 7400₽", callback_data="buy_8100_7400")]
     ]
 )
 
-# Кнопки со ссылкой на твой канал
+# Динамическая кнопка оплаты
+def payment_kb(url, invoice_id):
+    return InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(text="💳 Оплатить (CryptoBot)", url=url)],
+            [InlineKeyboardButton(text="✅ Проверить оплату", callback_data=f"check_{invoice_id}")]
+        ]
+    )
+
 social_kb = InlineKeyboardMarkup(
     inline_keyboard=[
         [InlineKeyboardButton(text="🎁 Ввести промокод", callback_data="act_promo")],
-        [InlineKeyboardButton(text="🔗 Перейти в канал", url="https://t.me/ad1skauc")]
+        [InlineKeyboardButton(text="🔗 Перейти в канал", url="https://t.me")]
     ]
 )
