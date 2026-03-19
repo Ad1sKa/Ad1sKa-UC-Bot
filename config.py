7e4a493e5511447a9df6b03498850603ff7a8df7
Ad1sKa <adskama828@gmail.com>
2026-03-19 19:24:13 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
 TOKEN = "8235277253:AAEe9fBNkgj9TTuCNeQ4Y_-9tIA4vbSCEZw"
 ADMIN_ID = 6762780884 
 SUPPORT_LINK = "ad1ska_pubg"
-# Получи этот токен в @BotFather (Bot Settings -> Payments -> Connect Test)
-PAYMENTS_TOKEN = "7490307358:TEST:h37TZrGbg1Lf4rs5" 
