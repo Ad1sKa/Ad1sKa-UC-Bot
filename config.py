d55da511627b1c7aafa76ec8c524847a9ee2fb7a
Ad1sKa <adskama828@gmail.com>
2026-06-05 13:12:13 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,15 @@
-TOKEN = "8235277253:AAEe9fBNkgj9TTuCNeQ4Y_-9tIA4vbSCEZw"
+import os
+
+# Основные настройки Telegram-бота
+# Получи НОВЫЙ токен в BotFather и вставь его сюда вместо старого!
+TOKEN = "8235277253:AAFZo5E7UHW6FaYb2YnNCmVDtAaCqGmQEkg" 
 ADMIN_ID = 6762780884 
-SUPPORT_LINK = "ad1ska_pubg"
+SUPPORT_LINK = "ad1ska_pubg" 
+
+# Настройки DonationAlerts API (Брать со скриншота)
+DA_APP_ID = 19267
+DA_API_KEY = "DBJMqTxS9dDo1lNiqdBnj3XEyJBmeJcfMaJM1QVF" # Твой секретный ключ
+
+# Ссылка на твою страницу доната (куда клиенты будут заходить для оплаты)
+# Замени 'ТВОЙ_НИК_НА_DA' на твой логин/никнейм на сайте DonationAlerts
+DA_DONATE_URL = "https://donationalerts.com" 
